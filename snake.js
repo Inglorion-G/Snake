ad7073fee47c75e6dda974d0c9db1817e4cbfe20
George Groh <george.groh@gmail.com>
2014-11-24 21:24:50 -0800

    All basic features workign

--- snake.js
+++ snake.js
@@ -2,6 +2,19 @@
 
 	var SnakeGame = root.SnakeGame = (root.SnakeGame || {});
 
+	var Apple = SnakeGame.Apple = function(pos) {
+		this.pos = pos;
+	};
+
+	Apple.prototype.draw = function(p) {
+		var pos = "#"+p[0]+"_"+p[1];
+		$(pos).addClass("apple");
+	};
+
+	Apple.prototype.remove = function() {
+		$(".tile").removeClass("apple");
+	}
+
 	var Snake = SnakeGame.Snake = function() {
 		this.head_pos = [4, 4];
 		this.body_positions = [[4, 3], [4, 2], [4, 1], [4, 0]];
@@ -37,22 +50,24 @@
 		}
 
 		this.draw();
-	}
+	};
 
 	Snake.prototype.draw = function() {
 		$(".tile").removeClass("snake");
 
-		$("#"+this.head_pos.join("_")).toggleClass("snake");
+		$("#"+this.head_pos.join("_")).addClass("snake");
 
 		if(this.body_length > 1) {
 			$.each(this.body_positions, function(idx, pos) {
-				$("#" + pos.join("_")).toggleClass("snake");
+				$("#" + pos.join("_")).addClass("snake");
 			});
 		}
-	}
+	};
 
 	var Game = SnakeGame.Game = function() {
-		this.paused = false;
+		this.paused = true;
+		this.stopped = false;
+		this.grow = null;
 	};
 
 	Game.prototype.setBoard = function() {
@@ -69,11 +84,13 @@
 				col ++;
 			}
 		}
-	}
+	};
 
-	Game.prototype.tick = function(n) {
+	Game.prototype.tick = function() {
+		this.growSnake();
 		this.snake.move();
-	}
+		this.checkCollisions();
+	};
 
 	Game.prototype.setDir = function(dir) {
 
@@ -101,10 +118,12 @@
 
 			default: return;
 		}
-	}
+	};
 
 	Game.prototype.run = function() {
 		var that = this;
+		this.randomApple();
+		this.apple.draw(this.apple.pos);
 
 		$(document).keydown(function(e) {
 			switch(e.which) {
@@ -125,12 +144,14 @@
 				break;
 
 				case 32:
-				if (!that.paused) {
+				if (that.paused) {
+					that.intervalID = setInterval(that.tick.bind(that), 120);
+					that.paused = false;
+					$(".info").hide();
+				} else {
 					clearInterval(that.intervalID);
 					that.paused = true;
-				} else {
-					that.intervalID = setInterval(that.tick.bind(that), 200);
-					that.paused = false;
+					$(".info").show();
 				}
 				break;
 
@@ -139,13 +160,62 @@
 			e.preventDefault();
 		});
 
-		this.intervalID = setInterval(this.tick.bind(this), 200);
-	}
+		//this.intervalID = setInterval(this.tick.bind(this), 120);
+	};
 
 	Game.prototype.addSnake = function() {
 
 		var snake = new Snake();
 		this.snake = snake;
+	};
+
+	Game.prototype.randomApple = function() {
+		var x = Math.floor(Math.random() * (15 - 0 + 1));
+		var y = Math.floor(Math.random() * (15 - 0 + 1));
+
+		this.apple = new Apple([x, y]);
+	};
+
+	Array.prototype.equals = function(array) {
+		for (var i = 0, l = array.length; i < l; i ++) {
+			if(this[i] !== array[i]) {
+				return false;
+			}
+		}
+
+		return true;
+	};
+
+	Game.prototype.checkCollisions = function() {
+		var head = this.snake.head_pos;
+
+		for(var i = 0, l = this.snake.body_positions.length; i < l; i ++) {
+			var body = this.snake.body_positions[i];
+			if(head.equals(body)) {
+				this.stopGame();
+			}
+		}
+
+		if(this.snake.head_pos.equals(this.apple.pos)) {
+			this.apple.remove();
+			this.randomApple();
+			this.apple.draw(this.apple.pos);
+			this.grow = (this.snake.body_positions[this.snake.body_positions.length - 1]).slice(0);
+		}
+	};
+
+	Game.prototype.growSnake = function() {
+		if (this.grow) {
+			this.snake.body_positions.push(this.grow);
+			this.snake.body_length ++;
+			this.grow = null;
+		}
 	}
+
+	Game.prototype.stopGame = function() {
+		clearInterval(this.intervalID);
+		this.stopped = true;
+		alert("You lost! Reload to try again!");
+	};
 
 })(this);
